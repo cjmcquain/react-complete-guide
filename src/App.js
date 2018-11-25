a8417354c09caa1e09815636172ecd7b33fdb65d
Charles McQuain <charlesmcquain@gmail.com>
2018-11-25 11:46:43 -0800

    Added deletePersonHandler to remove person when clicked on

--- src/App.js
+++ src/App.js
@@ -38,6 +38,12 @@ class App extends Component {
     console.log(doesShow);
   }
 
+  deletePersonHandler = (personIndex) => {
+    const persons = this.state.persons;
+    persons.splice(personIndex, 1);
+    this.setState({ persons: persons })
+  }
+
   render() {
     const style = {
       backgroundColor: 'white',
@@ -52,8 +58,11 @@ class App extends Component {
     if (this.state.showPersons) {
       persons = (
         <div>
-          {this.state.persons.map(person => {
-            return <Person name={person.name} age={person.age} />
+          {this.state.persons.map((person, index) => {
+            return <Person
+              click={() => this.deletePersonHandler(index)}
+              name={person.name}
+              age={person.age} />
           })}
         </div>
       );
