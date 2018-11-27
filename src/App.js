4e6ec267f2a0b12b914537dd0682340fe05cca3e
Charles McQuain <charlesmcquain@gmail.com>
2018-11-26 20:41:17 -0800

    Edited nameChangeHandler

--- src/App.js
+++ src/App.js
@@ -22,13 +22,22 @@ class App extends Component {
     });
   }
 
-  nameChangedHandler = (event) => {
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    };
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
     this.setState({
-      persons: [
-        { name: 'Max', age: 35 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 28 }
-      ]
+      persons: persons
     });
   }
 
@@ -63,6 +72,7 @@ class App extends Component {
               name={person.name}
               age={person.age}
               key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)}
             />
           })}
         </div>
